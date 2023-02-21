0c5f243a1d75273465bef6798ecb2ccf41295074
Wixonic <public.wixonic@icloud.com>
2023-02-21 15:06:12 +0100

    Update

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const fs = require("fs");
+const audioconcat  = require("audioconcat");
+
 const express = require("express");
+const { instrument } = require("@socket.io/admin-ui");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = require("http").createServer(app);
-const io = new (require("socket.io").Server)(server,{
+const io = new Server(server,{
 	connectTimeout: 5000,
 	pingInterval: 5000,
 	pingTimeout: 10000,
@@ -16,32 +21,42 @@ const io = new (require("socket.io").Server)(server,{
 	serveClient: false,
 
 	cors: {
-		origin: true
+		origin: ["https://admin.socket.io"],
+		credentials: true
 	}
 });
 
-const manager = {
-	ips: {}
+class User {
+	static list = {};
+
+	constructor (socket) {
+		this.id = socket.id;
+		this.position = {
+			x: 0,
+			y: 0,
+			z: 0
+		};
+	};
 };
 
-io.on("connection",(socket) => {
-	if (manager.ips[socket.request.socket.remoteAddress] instanceof Array) {
-		manager.ips[socket.request.socket.remoteAddress].push(socket.id);
-	} else {
-		manager.ips[socket.request.socket.remoteAddress] = [socket.id];
-	}
+io.on("connection",async (socket) => {
+	User.list[socket.id] = new User(socket);
 	
-	socket.on("disconnecting",() => {
-		manager.ips[socket.request.socket.remoteAddress].splice((manager.ips[socket.request.socket.remoteAddress] || []).indexOf(socket.id),1);
-	});
-
 	socket.on("ping",(callback) => callback(Date.now()));
+	socket.on("voice",(data) => io.of("/").emit("voice",socket.id,data));
+
+	setInterval(() => io.of("/").emit("list",User.list),100);
 });
 
 app.get("/",(_,res) => {
 	res.redirect("https://github.com/Wixonic/Among-Sus-Server/pkgs/npm/among-sus-server#readme");
 });
 
+instrument(io,{
+	auth: false,
+	mode: "development"
+});
+
 server.listen(3000,() => {
-	console.info("Server is listening");
+	console.log("Server is listening on " + server.address().address + ":" + server.address().port);
 });
