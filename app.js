69aa43ecf6cd5ffac98a54ae7daaa93200f53b4c
Wixonic <public.wixonic@icloud.com>
2023-02-24 17:06:48 +0100

    Update

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const { instrument } = require("@socket.io/admin-ui");
 const { Server } = require("socket.io");
 
 const app = express();
-const server = require("http").createServer(app);
+const server = require("https").createServer({
+	key: fs.readFileSync("key.pem"),
+	cert: fs.readFileSync("cert.pem")
+},app);
+
 const io = new Server(server,{
 	connectTimeout: 5000,
 	pingInterval: 5000,
@@ -21,7 +25,7 @@ const io = new Server(server,{
 	serveClient: false,
 
 	cors: {
-		origin: ["https://admin.socket.io"],
+		origin: ["http://localhost:5050","https://wixonic-among-sus.web.app","https://sus.wixonic.fr","https://admin.socket.io"],
 		credentials: true
 	}
 });
@@ -41,22 +45,21 @@ class User {
 
 io.on("connection",async (socket) => {
 	User.list[socket.id] = new User(socket);
-	
-	socket.on("ping",(callback) => callback(Date.now()));
-	socket.on("voice",(data) => io.of("/").emit("voice",socket.id,data));
+
+	io.of("/").except(socket.id).emit("signal",socket.id,0);
+
+	socket.on("signal",(to,state,...args) => io.of("/").in(to).emit("signal",socket.id,state,...args));
 
 	setInterval(() => io.of("/").emit("list",User.list),100);
+
+	socket.on("ping",(callback) => callback(Date.now()));
 });
 
-app.get("/",(_,res) => {
-	res.redirect("https://github.com/Wixonic/Among-Sus-Server/pkgs/npm/among-sus-server#readme");
-});
+app.get("/",(_,res) => res.redirect("https://github.com/Wixonic/Among-Sus-Server/pkgs/npm/among-sus-server#readme"));
 
 instrument(io,{
 	auth: false,
 	mode: "development"
 });
 
-server.listen(3000,() => {
-	console.log("Server is listening on " + server.address().address + ":" + server.address().port);
-});
+server.listen(3000,() => console.log("Server is listening on " + server.address().address + ":" + server.address().port));
